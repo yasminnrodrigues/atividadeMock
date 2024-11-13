e6da3920ff4aed2323d96e2d1a56346d06b01a12
yasminnrodrigues <151582876+yasminnrodrigues@users.noreply.github.com>
2024-11-13 10:55:04 -0300

    Update appTest.java

--- Atividade/src/test/java/appTest.java
+++ Atividade/src/test/java/appTest.java
@@ -23,14 +23,14 @@ public class AppTest {
     public void SalvarImagemCorretamente() {
         Product product = new Product(1); // Crie uma instância real do produto
         application.append(product);
-        Mockito.verify(service, Mockito.times(1)).save(Mockito.eq(product)); // Verifica o produto passado
+        Mockito.verify(service, Mockito.times(1)).save(Mockito.eq(product)); 
     }
 
     @Test
     public void DeletarImagemCorretamente() {
         int productId = 1; // ID específico
         application.remove(productId);
-        Mockito.verify(service, Mockito.times(1)).remove(Mockito.eq(productId)); // Verifica o ID passado
+        Mockito.verify(service, Mockito.times(1)).remove(Mockito.eq(productId)); 
     }
 
     @Test
@@ -38,6 +38,6 @@ public class AppTest {
         Product product = new Product(); // Crie uma instância real do produto
         int productId = 1; // ID específico
         application.update(productId, product);
-        Mockito.verify(service, Mockito.times(1)).update(Mockito.eq(product)); // Verifica o produto passado
+        Mockito.verify(service, Mockito.times(1)).update(Mockito.eq(product)); 
     }
 }
