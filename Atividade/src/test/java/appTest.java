993ba3e16fd38bf2fdca44b3e21f16b8f7601db0
yasminnrodrigues <151582876+yasminnrodrigues@users.noreply.github.com>
2024-11-13 20:19:32 -0300

    Update appTest.java

--- Atividade/src/test/java/appTest.java
+++ Atividade/src/test/java/appTest.java
@@ -1,4 +1,4 @@
-package com.example.applications;
+import com.example.applications.ProductApplication;
 import com.example.entities.Product;
 import com.example.services.ProductService;
 import org.junit.jupiter.api.Test;
@@ -11,8 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
-public class AppTest {
-
+public class ProductApplicationTest {
     @InjectMocks
     ProductApplication application;
 
@@ -22,22 +21,27 @@ public class AppTest {
     @Test
     public void SalvarImagemCorretamente() {
         Product produto = any(Product.class);
-        application.append(product);
-        Mockito.verify(service, Mockito.times(1)).save(product);
+
+        application.append(produto);
+
+        Mockito.verify(service, Mockito.times(1)).save(produto);
     }
 
     @Test
     public void DeletarImagemCorretamente() {
         Product produto = any(Product.class);
+
         application.remove(1);
+
         Mockito.verify(service, Mockito.times(1)).remove((1));
     }
 
     @Test
     public void AtualizarImagemCorretamente() {
-        Product product = new Product(1);
-        int productId = 1;
-        application.update(1, product);
-        Mockito.verify(service, Mockito.times(1)).update((product));
+        Product produto = any(Product.class);
+
+        application.update(1, produto);
+
+        Mockito.verify(service, Mockito.times(1)).update((produto));
     }
 }
